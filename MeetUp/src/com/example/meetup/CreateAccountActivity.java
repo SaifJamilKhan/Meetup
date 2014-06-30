33b68b7b7cbed7cc397149f4afdb7325a8b4476a
Saif <sj5khan@uwaterloo.ca>
2014-06-29 21:39:11 -0400

    UI for CreateAccountActivity. App successfully creates accounts =)

--- MeetUp/src/com/example/meetup/CreateAccountActivity.java
+++ MeetUp/src/com/example/meetup/CreateAccountActivity.java
@@ -1,13 +1,67 @@
 package com.example.meetup;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CreateAccountActivity extends Activity {
+import com.example.meetup.NetworkRequestUtil.NetworkRequestListener;
+
+public class CreateAccountActivity extends Activity implements
+		NetworkRequestListener {
+
+	private EditText mUserNameText;
+	private EditText mPasswordText;
+	private EditText mPasswordConfirmText;
+	private EditText mEmailText;
+	private Button mCreateAccountButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_account);
+
+		mUserNameText = (EditText) findViewById(R.id.create_account_user_name_text);
+		mPasswordText = (EditText) findViewById(R.id.create_account_password_text);
+		mPasswordConfirmText = (EditText) findViewById(R.id.create_account_password_confirm_text);
+		mEmailText = (EditText) findViewById(R.id.create_account_email_text);
+
+		mCreateAccountButton = (Button) findViewById(R.id.send_create_account_button);
+		mCreateAccountButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				JSONObject body = new JSONObject();
+				JSONObject user = new JSONObject();
+				try {
+					user.put("email", mEmailText.getText());
+					user.put("password", mPasswordText.getText());
+					user.put("password_confirmation",
+							mPasswordConfirmText.getText());
+					user.put("name", mUserNameText.getText());
+					body.put("user", user);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				NetworkRequestUtil.makePostRequest("registrations",
+						CreateAccountActivity.this, body);
+			}
+		});
+	}
+
+	@Override
+	public void requestSucceededWithJSON() {
+
+	}
+
+	@Override
+	public void requestFailed() {
+
 	}
 }
--- MeetUp/src/com/example/meetup/NetworkRequestUtil.java
+++ MeetUp/src/com/example/meetup/NetworkRequestUtil.java
@@ -1,49 +1,100 @@
 package com.example.meetup;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
 
 public class NetworkRequestUtil {
+	private static String baseUrl = "http://meet-up-server.herokuapp.com/";
 
-	public static void makePostRequest() {
-		DefaultHttpClient httpclient = new DefaultHttpClient(
-				new BasicHttpParams());
-		HttpPost httppost = new HttpPost("http://someJSONUrl/jsonWebService");
-		// Depends on your web service
-		httppost.setHeader("Content-type", "application/json");
+	private static class RequestTask extends AsyncTask<String, String, String> {
+		NetworkRequestListener listener;
+		JSONObject body;
 
-		InputStream inputStream = null;
-		String result = null;
-		try {
-			HttpResponse response = httpclient.execute(httppost);
-			HttpEntity entity = response.getEntity();
-
-			inputStream = entity.getContent();
-			// json is UTF-8 by default
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					inputStream, "UTF-8"), 8);
-			StringBuilder sb = new StringBuilder();
-
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			result = sb.toString();
-		} catch (Exception e) {
-			// Oops
-		} finally {
-			try {
-				if (inputStream != null)
-					inputStream.close();
-			} catch (Exception squish) {
-			}
+		public RequestTask(NetworkRequestListener listener, JSONObject body) {
+			this.listener = listener;
+			this.body = body;
 		}
+
+		@Override
+		protected String doInBackground(String... uri) {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response;
+			String responseString = null;
+			try {
+				HttpPost post = new HttpPost(uri[0]);
+				post.setHeader("Accept", "application/json");
+				post.setHeader("Content-type", "application/json");
+				StringEntity entity = new StringEntity(this.body.toString());
+				entity.setContentType("application/json");
+				entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+						"application/json"));
+
+				post.setEntity(entity);
+				response = httpclient.execute(post);
+				StatusLine statusLine = response.getStatusLine();
+				if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					response.getEntity().writeTo(out);
+					out.close();
+					responseString = out.toString();
+					try {
+						convertToJson(responseString);
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					// Closes the connection.
+					response.getEntity().getContent().close();
+					throw new IOException(statusLine.getReasonPhrase());
+				}
+			} catch (ClientProtocolException e) {
+				// TODO Handle problems..
+			} catch (IOException e) {
+				// TODO Handle problems..
+			}
+			return responseString;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			// Do anything with response..
+		}
+	}
+
+	public static interface NetworkRequestListener {
+
+		public abstract void requestSucceededWithJSON();
+
+		public abstract void requestFailed();
+	}
+
+	public static void makePostRequest(String path,
+			NetworkRequestListener listener, JSONObject body) {
+		RequestTask task = new RequestTask(listener, body);
+		task.execute(baseUrl + path);
+	}
+
+	public static void convertToJson(String responseString)
+			throws JSONException {
+		JSONObject jObject = new JSONObject(responseString);
+		Log.v("json", jObject.toString());
 	}
 }
